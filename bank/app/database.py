d387ce974e32d9a3889f9850328820aa74d71b3a
HEMANTH KUMAR THIPPAVATHI <hemanthkumarthippavathi@gmail.com>
2026-07-31 22:27:49 +0530

    Refactor database URL to use environment variable
    Updated DATABASE_URL to use environment variable for password.

--- bank/app/database.py
+++ bank/app/database.py
@@ -1,17 +1,23 @@
 import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:minnie@localhost:3306/bank_management")
-# DATABASE_URL= mysql+pymysql://avnadmin:AVNS_TNgAshCoHJKnC2GQhHb@mysql-39501748-hemanthkumarthippavathi-b4e3.g.aivencloud.com:16337/defaultdb?ssl-mode=REQUIRED
-DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_TNgAshCoHJKnC2GQhHb@mysql-39501748-hemanthkumarthippavathi-b4e3.g.aivencloud.com:16337/defaultdb"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://avnadmin:YOUR_PASSWORD@mysql-39501748-hemanthkumarthippavathi-b4e3.g.aivencloud.com:16337/defaultdb"
+)
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {},
+    pool_pre_ping=True,
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
 Base = declarative_base()
 
 
