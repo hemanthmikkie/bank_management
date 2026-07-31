90f023e52b8c45739c1d477a0f9259fba6b1cef0
HEMANTH KUMAR THIPPAVATHI <hemanthkumarthippavathi@gmail.com>
2026-07-31 10:50:00 +0530

    Fix DATABASE_URL assignment format
    Updated DATABASE_URL assignment to use quotes.

--- bank/app/database.py
+++ bank/app/database.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:minnie@localhost:3306/bank_management")
-DATABASE_URL= mysql+pymysql://avnadmin:AVNS_TNgAshCoHJKnC2GQhHb@mysql-39501748-hemanthkumarthippavathi-b4e3.g.aivencloud.com:16337/defaultdb?ssl-mode=REQUIRED
+# DATABASE_URL= mysql+pymysql://avnadmin:AVNS_TNgAshCoHJKnC2GQhHb@mysql-39501748-hemanthkumarthippavathi-b4e3.g.aivencloud.com:16337/defaultdb?ssl-mode=REQUIRED
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_TNgAshCoHJKnC2GQhHb@mysql-39501748-hemanthkumarthippavathi-b4e3.g.aivencloud.com:16337/defaultdb?ssl-mode=REQUIRED"
+
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {},
